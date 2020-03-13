fbd1b36ebc348c6066328e350119047e7d2fae82
srtr <srtr@outlook.com>
2020-03-13 17:07:56 -0400

    add name and mobile for login validation

--- src/login.py
+++ src/login.py
@@ -1,7 +1,18 @@
+import re
+
 class Login:
     def validate(self, form):
-        email = form['email']
-        if not isinstance(email, str):
+        if 'name' not in form or 'mobile' not in form or 'email' not in form:
             return False
-        else:
-            return True
+
+        email = form['email']
+        name = form['name']
+        mobile = form['mobile']
+
+        if not isinstance(email, str) or not isinstance(name, str) or not name or not email:
+            return False
+
+        if len(email) > 7:
+            if re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
+                return True
+        return False
