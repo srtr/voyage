1b20c670bce7dcfd73580c9a5e4fba37bd79df1b
srtr <srtr@outlook.com>
2020-03-13 17:36:26 -0400

    refactoring + pr review changes

--- src/login.py
+++ src/login.py
@@ -1,18 +1,46 @@
 import re
 
 class Login:
-    def validate(self, form):
-        if 'name' not in form or 'mobile' not in form or 'email' not in form:
+
+    def validate_email(self,email):
+        if not isinstance(email, str) or not email:
             return False
+
+        if len(email) > 7:
+            
+            if re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
+                return True
+
+        return False
+
+    def validate_name(self,name):
+        if not isinstance(name, str) or not name:
+            return False
+
+        return True
+    
+    def validate_mobile(self,mobile):
+        if not isinstance(mobile, str):
+            return False
+        
+        if len(mobile) == 10:
+            
+            if re.match('^[0-9]+$', mobile) != None:
+                return True
+
+        return False
+
+    def validate(self, form):
+        keys = ['name','mobile','email']
+
+        for key in keys:
+            if key not in form:
+                return False
 
         email = form['email']
         name = form['name']
         mobile = form['mobile']
+        
+        return self.validate_email(email) and self.validate_name(name) and self.validate_mobile(mobile)
 
-        if not isinstance(email, str) or not isinstance(name, str) or not name or not email:
-            return False
-
-        if len(email) > 7:
-            if re.match("^.+@(\[?)[a-zA-Z0-9-.]+.([a-zA-Z]{2,3}|[0-9]{1,3})(]?)$", email) != None:
-                return True
-        return False
+        
--- test/test_login.py
+++ test/test_login.py
@@ -3,7 +3,7 @@ from src.login import Login
 
 class LoginTest(TestCase):
     def test_login_passes_with_valid_form(self):
-        form = {'email': 'validemail@email.com', 'name': 'customer_1', 'mobile': 9000000000}
+        form = {'email': 'validemail@email.com', 'name': 'customer_1', 'mobile': '9876543210'}
         self.assertEqual(Login().validate(form), True)
 
     def test_login_fails_with_numeric_emailId(self):
@@ -12,19 +12,19 @@ class LoginTest(TestCase):
         self.assertEqual(Login().validate(form), False)
 
     def test_login_fails_with_invalid_emailId(self):
-        form = {'email': 'zzzz', 'name': 'customer_1', 'mobile': 9000000000}
+        form = {'email': 'zzzz', 'name': 'customer_1', 'mobile': '9876543210'}
         self.assertEqual(Login().validate(form), False)
 
     def test_login_fails_with_missing_emailId(self):
-        form = {'name': 'customer_1', 'mobile': 9000000000}
+        form = {'name': 'customer_1', 'mobile': '9876543210'}
         self.assertEqual(Login().validate(form), False)
 
     def test_login_fails_with_missing_name(self):
-        form = {'email': 'abcdef@example.com', 'mobile': 9000000000}
+        form = {'email': 'abcdef@example.com', 'mobile': '9876543210'}
         self.assertEqual(Login().validate(form), False)
 
     def test_login_fails_with_missing_mobile(self):
-        form = {'email': 'abcdef@example.com', 'name': 'customer_1',}
+        form = {'email': 'abcdef@example.com', 'name': '9876543210',}
         self.assertEqual(Login().validate(form), False)
 
 
